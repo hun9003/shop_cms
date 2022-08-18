e7fd2ceb9b4f5e0a39bfbb08f3c0c8b3da7d8c52
hun9003 <hun9003@naver.com>
2022-08-18 18:14:57 +0900

    ADD : JpaAuditingConfiguration.java

---
+++ src/main/java/com/rateye/shop_cms/config/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package com.rateye.shop_cms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfiguration {
+}
