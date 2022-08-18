a4a9af68a014ed525f9f068f2452698447d3f78d
hun9003 <hun9003@naver.com>
2022-08-18 16:02:11 +0900

    Initial commit

---
+++ src/test/java/com/rateye/shop_cms/ShopCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rateye.shop_cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopCmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
